428c1d4e1c4375f53b411f2e0e641038b4f96677
Andrewpqc <m13636038496@163.com>
2018-03-13 23:35:21 +0800

    bug fix about cache

--- service/api.py
+++ service/api.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 from aiohttp import web
 from aiohttp.web import Response
@@ -7,31 +8,53 @@ from .redis import redis_client
 
 api = web.Application()
 
+
 # ====== async view handlers ======
 @require_info_login
 async def grade_all_api(request, s, sid, ip):
+    current_date = datetime.datetime.now().date()
+    current_year = current_date.year
     query_string = request.rel_url.query_string
     if query_string:
-        keys = []; vals = []
+        keys = []
+        vals = []
         for _ in query_string.split('&'):
             keys.append(_.split('=')[0])
             vals.append(_.split('=')[1])
         args = dict(zip(keys, vals))
-        #xqm:第一学期３，第二学期12
-        # xnm: 学年 2016,2017...　
-        xnm = args.get('xnm'); xqm = args.get('xqm')
-        key=sid+"_"+xnm+"_"+xqm
-        val=redis_client.get(key)
-        if val is not None:
-            return web.json_response(json.loads(val))
-        else:
+        #xqm:第一学期３，第二学期12,第三学期16
+        #xnm: 学年 2016,2017...
+        xnm = args.get('xnm')
+        xqm = args.get('xqm')
+        if (int(xnm) + 1 == current_year and xqm == "3"
+                and datetime.date(current_year, 1, 15) < current_date <
+                datetime.date(current_year, 2, 28)) or (
+                    int(xnm) + 1 == current_year and xqm != "3"
+                    and datetime.date(current_year, 6, 1) < current_date <
+                    datetime.date(current_year, 9, 1)):
             gradeList = await get_grade(s, sid, ip, xnm, xqm)
             if gradeList:
-                redis_client.set(key,json.dumps(gradeList),ex=126144000)#过期时间为4年
                 return web.json_response(gradeList)
             else:
-                return Response(body=b'', content_type='application/json', status=403)
-    # =================================
+                return Response(
+                    body=b'', content_type='application/json', status=403)
+        else:
+            key = sid + "_" + xnm + "_" + xqm
+            val = redis_client.get(key)
+            if val is not None:
+                return web.json_response(json.loads(val))
+            else:
+                gradeList = await get_grade(s, sid, ip, xnm, xqm)
+                if gradeList:
+                    redis_client.set(
+                        key, json.dumps(gradeList), ex=126144000)  #过期时间为4年
+                    return web.json_response(gradeList)
+                else:
+                    return Response(
+                        body=b'', content_type='application/json', status=403)
+
+
+# =================================
 
 # ====== url --------- maps  ======
 api.router.add_route('GET', '/grade/', grade_all_api, name='grade_all_api')
