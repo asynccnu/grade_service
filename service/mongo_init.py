a160763e03828eab31906030621163ba46ddcc3d
Darren <am.darren.t@gmail.com>
2019-05-19 04:17:42 +0800

    fix: add mongodb auth

--- service/mongo_init.py
+++ service/mongo_init.py
@@ -6,10 +6,12 @@ MONGO_HOST=os.environ.get("MONGO_HOST") or "127.0.0.1"
 MONGO_PORT=os.environ.get("MONGO_PORT") or 27017
 MONGO_ACCOUNT=os.environ.get("MONGO_ACCOUNT") or None
 MONGO_PASSWORD=os.environ.get("MONGO_PASSWORD") or None
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME') or "muxi"
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD') or "nopassword"
 
-
-client = MongoClient(MONGO_HOST, int(MONGO_PORT))
-
+# client = AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
+mongo_uri = "mongodb://{}:{}@{}:{}".format(MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT)
+client = AsyncIOMotorClient(mongo_uri)
 #连接gradedb数据库,账号密码认证
 db = client.gradedb
 
