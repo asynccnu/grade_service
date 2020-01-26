7b41982beb7860331b20fb67983323aab29758e7
Humbertzhang <504490160@qq.com>
2020-01-26 14:22:19 +0800

    Update mongo_init.py

--- service/mongo_init.py
+++ service/mongo_init.py
@@ -2,15 +2,10 @@ import os
 from pymongo import MongoClient
 
 #建立和数据库系统的连接,指定host及port参数
-MONGO_HOST=os.environ.get("MONGO_HOST") or "127.0.0.1"
-MONGO_PORT=os.environ.get("MONGO_PORT") or 27017
-MONGO_ACCOUNT=os.environ.get("MONGO_ACCOUNT") or None
-MONGO_PASSWORD=os.environ.get("MONGO_PASSWORD") or None
-MONGODB_USERNAME = os.getenv('MONGODB_USERNAME') or "muxi"
-MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD') or "nopassword"
+MONGO_URI=os.environ.get("MONGO_URI") or "mongodb://username:secret@localhost:27017/?authSource=admin"
 
 # client = AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
-mongo_uri = "mongodb://{}:{}@{}:{}".format(MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT)
+mongo_uri = MONGO_URI
 client = AsyncIOMotorClient(mongo_uri)
 #连接gradedb数据库,账号密码认证
 db = client.gradedb
